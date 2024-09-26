5798943d0f668f1de6b0cf6244824b7a85654a96
Shalev Habany <shalev.habany@gmail.com>
2024-09-26 20:09:26 +0300

    dirs refactor

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 from tqdm import tqdm
 
-from src.arithmetic_coding_decoder import Decompressor
-from src.arithmetic_coding_encoder import Compressor
+from src.arithmetic_coding.arithmetic_coding_decoder import Decompressor
+from src.arithmetic_coding.arithmetic_coding_encoder import Compressor
 
 if __name__ == '__main__':
     with tqdm(total=2, desc="Compression and Decompression", colour="green") as pbar:
         compressor = Compressor(32, 15)
-        compressor.compress_file('dickens.txt', 'compressed.bin')
+        compressor.compress_file('./data/dickens.txt', './data/compressed.bin')
         pbar.update(1)
 
         decompressor = Decompressor(15)
-        decompressor.decompress_file('compressed.bin', 'decompressed.txt', 'dickens.txt')
+        decompressor.decompress_file('./data/compressed.bin', './data/decompressed.txt', './data/dickens.txt')
         pbar.update(1)
